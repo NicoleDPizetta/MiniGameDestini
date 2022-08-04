55e022e56b32f24029c6c80edf815b16f0ca5d2a
NicoleDPizetta <nicoledalzotto@outlook.com>
2022-08-04 17:40:46 -0300

    finished project section 10

---
+++ Destini-iOS13/Model/Story.swift
@@ -0,0 +1,20 @@
+//
+//  Sotry.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+//story0 = "You see a fork in the road."
+//let choice1 = "Take a left."
+//let choice2 = "Take a right."
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
